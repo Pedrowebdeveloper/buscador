51432de9d6817662c69b1d7c264f25479584b869
Pedro <pedromello172@hotmail.com>
2022-09-14 20:55:58 -0300

    conclusão do projeto buscador de cep

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
+import api from './services/api';
 
 function App() {
 
   const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
 
-  function handleSearch() {
-    alert(input);
+  async function handleSearch() {
+    if(input === ''){
+      alert('Preeencha algum CEP!')
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert("Ops erro ao buscar")
+      setInput('')
+    }
   }
 
   return (
@@ -27,14 +41,18 @@ function App() {
         </button>
       </div>
       
-      <main className='main'>
-        <h2>CEP: 79003222</h2>
-
-        <span>Rua teste algum</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Vila Rosa</span>
-        <span>Campo Grande - MS</span>
-      </main>  
+      
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+  
+          <span>{cep.logradouro}</span>
+          <span>Complemento:{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+        
 
     </div>
   );
